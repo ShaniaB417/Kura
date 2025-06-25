d4e0a3436e9ec3e65d61ac14ffecc584d6eca03f
ShaniaB417 <107355668+ShaniaB417@users.noreply.github.com>
2025-06-24 20:01:11 -0700

    Initial Commit

---
+++ Kura/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Kura
+//
+//  Created by Shania Brown on 6/24/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Kura/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Kura
+//
+//  Created by Shania Brown on 6/24/25.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            ToggleImmersiveSpaceButton()
+        }
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
